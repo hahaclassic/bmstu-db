960dabc74958e2026a686c2c1e056899b4089921
hahaclassic <gavrilyukvlad221@gmail.com>
2024-10-05 02:03:01 +0300

    02: added4 queries

---
+++ 02_main_dml_instructions/sql/queries.sql
@@ -0,0 +1,38 @@
+-- 1. Инструкция SELECT, использующая предикат сравнения. 
+-- Получение альбомов исполнителей из Франции.
+SELECT DISTINCT 
+	albums.id AS album_id, 
+    albums.title AS album_title, 
+    artists.name AS artist_name
+FROM albums
+INNER JOIN albums_by_artists ON albums.id = albums_by_artists.album_id
+INNER JOIN artists ON albums_by_artists.artist_id = artists.id
+WHERE artists.country = 'France';
+
+
+-- 2. Инструкция SELECT, использующая предикат BETWEEN. 
+-- Получение треков, у которых от 100к до 200к прослушиваний и они explicit.
+select distinct t.name, t.genre, t.stream_count from tracks as t 
+where t.stream_count between 100000 and 200000 and t.explicit;
+
+
+-- 3. Инструкция SELECT, использующая предикат LIKE. 
+-- Получение всех треков, у которых в названии есть слово 'book'
+select distinct t.id as track_id, t.name as track_name from tracks as t where t.name like '%book%';
+
+-- 4. Инструкция SELECT, использующая предикат IN с вложенным подзапросом
+-- Получение альбомов исполнителей из Франции
+SELECT id, title
+FROM albums
+WHERE id IN (
+    SELECT album_id
+    FROM albums_by_artists
+    WHERE artist_id IN (
+        SELECT id
+        FROM artists
+        WHERE country = 'France'
+    )
+);
+
+-- 5. Инструкция SELECT, использующая предикат EXISTS с вложенным подзапросом.
+
